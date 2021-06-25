894d0e527c80063dd3b580a07e7dec7c562bb662
Kash <kserenitym@gmail.com>
2021-06-25 15:01:21 -0500

    setting up server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app= express();
+const PORT = 5000;
+
+app.use(express.static('server/public'));
+
+app.listen( port, () =>{
+    console.log(`listening on port ${PORT}`)
+});
+
