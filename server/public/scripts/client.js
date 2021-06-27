ae537217141bb77e9bb0381f21f4841c1b522615
Kash <kserenitym@gmail.com>
2021-06-27 00:22:56 -0500

    operator values have been assigned in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,62 +2,82 @@ $(document).ready(onReady);
 
 
 function onReady(){
-
+    console.log("let's get started!");
     //empty input values
-    $('#clearInput').on('click', function(){
-        $('#numberOne').val("");
-        $('#numberTwo').val("");
-    })
+    $('#clearInput').on('click', clearInputs)
+    $('#equalsBtn').on("click", createEquation)
+    $('#additionOperator').on('click', additionBtn)
+    $('#subtractOperator').on('click', subtractBtn)
+    $('#divideOperator').on('click', divisionBtn)
+    $('#multiplyOperator').on('click', multiplyBtn)
 }
 
-function equation(){
-    //create string 
-    let equationObj={
-        number1: $('#numberOne').val(),
-        number2: $('#secondNumber').val(),
-        operator: operator
-    }  
-    console.log('in equation', equationObj);
+let operator='';
+
+function additionBtn(){
+    console.log('clicked additionBtn');
+    operator= '+';
+}
+
+function subtractBtn(){
+    operator= '-';
+}
+
+function divisionBtn(){
+    operator= '/';
+}
+
+function multiplyBtn(){
+    operator= '*';
+}
+
+function createEquation(){
+    console.log('in createEquation... to be sent to server', operator);
+    //create string and send it to server
+    
+    // console.log('objects', equationObj);
+}
+
+
+function clearInputs(){
+    $('#numberOne').val("");
+    $('#numberTwo').val("");
+    operator='';
 }
 
 
 
-function postCalc(equationObj){
+
+
+function sendCalc(equationObj){
+    //sending equationObj to url /calculations
      $.ajax({
         type: 'POST',
         url:'/calculations',
         data: equationObj
     }) .then (function(res){
         console.log('Response is', res)
-        for( let i=0; i < res.length ; i++){
-            let problem= res[i];
-            $('#calcHistory').append(`
-            <li> ${problem.} </li>
-            `)
-        }
-    })
         
-}
+    })
+
+}    
    
 
 
 
 
-function showHistory(){
+function showHistory(response){
     $.ajax({
         type: 'GET',
         url: "/calculations"
-        data: Equation(firstNumber,secondNumber,operatorComp)
-
     }).then (function(res){
+        let showEquation=0;
         console.log('Response is', res)
         for( let i=0; i < res.length ; i++){
             let problem= res[i];
             $('#calcHistory').append(`
-            <li> ${problem.} </li>
-            `)
-
-    
+            <li> ${problem.number1} ${problem.operator} ${problem.number2} = ${problem.answer}</li>
+            `);
+        }
+    });
 }
-
-function 
