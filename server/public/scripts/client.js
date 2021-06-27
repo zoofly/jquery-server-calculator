fabb8c3f194d24bfa8b7d09abe63461ad5ed991e
Kash <kserenitym@gmail.com>
2021-06-26 20:26:54 -0500

    wokring on server routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,36 @@
-function showHistory(){
-    
+$(document).ready(onReady);
 
-    //.ajax
+
+function onReady(){
+
+    //empty input values
+    $('#clearInput').on('click', function(){
+        $('#numberOne').val("");
+        $('#numberTwo').val("");
+    })
 }
+
+function Equation(firstNumber, secondNumber, operatorComp){
+    this.numbOne= firstNumber
+    this.numbTwo= secondNumber
+    this.operator= operatorComp
+    $.ajax({
+        type: 'GET',
+        url:'/calculations',
+        data: 
+
+    })
+}
+
+
+// function showHistory(){
+
+//     $.ajax({
+//         type: 'POST',
+//         url: "/calculations"
+//         data: Equation(firstNumber,secondNumber,operatorComp)
+
+//     })
+
+    
+// }
