0486d50e75bc6eb395e57c10643bc5cd2483fef0
Kash <kserenitym@gmail.com>
2021-06-26 23:42:42 -0500

    clear inputs working

--- server/server.js
+++ server/server.js
@@ -1,14 +1,49 @@
 const express = require('express');
 const app= express();
 const PORT = 5000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('server/public'));
 
-app.listen( port, () =>{
+app.listen( PORT, () =>{
     console.log(`listening on port ${PORT}`)
 });
 
-function getCalculation(){
-    let calculation = [];
+
+app.get('/history', (req,res)=>{
+    console.log('History of calculations');
+    res.send(pastCalculations)
+});
+121
+let pastCalculations = [];
+app.get('/calculations', (req,res)=>{
+    let firstNumber= req.body.firstNumber;
+    let secondNumber= req.body.secondNumber;
+    let operator = req.body.operator;
+    function getCalculation(equation){
+    switch (operator) {
+        case '+':
+            answer= Number(firstNumber)+ Number(secondNumber)
+            
+            break;
+
+        case '-':
+            answer= Number(firstNumber)- Number(secondNumber)
+             break;
+
+         case '/':
+            answer= Number(firstNumber)/ Number(secondNumber)
+            break;
+        case '*':
+            answer= Number(firstNumber)* Number(secondNumber)
+            break;
     
-}
+        default: null;
+            break;
+        }
+    }
+    pastCalculations.push(equationObj);
+    res.send(equationObj);
+});
